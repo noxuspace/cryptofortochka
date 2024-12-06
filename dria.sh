c8718f0b98f5b2a9d1727140373d9886a9258bbd
NOXU <noxu.space@gmail.com>
2024-12-06 17:59:14 +0200

    Create dria.sh

---
+++ dria.sh
@@ -0,0 +1,165 @@
+#!/bin/bash
+
+# Цвета текста
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m' # Нет цвета (сброс цвета)
+
+# Проверка наличия curl и установка, если не установлен
+if ! command -v curl &> /dev/null; then
+    sudo apt update
+    sudo apt install curl -y
+fi
+sleep 1
+
+# Отображаем логотип
+curl -s https://raw.githubusercontent.com/noxuspace/cryptofortochka/main/logo_forto.sh | bash
+
+# Меню
+echo -e "${YELLOW}Выберите действие:${NC}"
+echo -e "${CYAN}1) Установка ноды${NC}"
+echo -e "${CYAN}2) Запуск ноды${NC}"
+echo -e "${CYAN}3) Обновление ноды${NC}"
+echo -e "${CYAN}4) Изменение порта${NC}"
+echo -e "${CYAN}5) Проверка логов${NC}"
+echo -e "${CYAN}6) Удаление ноды${NC}"
+
+echo -e "${YELLOW}Введите номер:${NC} "
+read choice
+
+case $choice in
+    1)
+        echo -e "${BLUE}Устанавливаем ноду Dria...${NC}"
+
+        # Обновление и установка зависимостей
+        sudo apt update && sudo apt-get upgrade -y
+        sudo apt install git make jq build-essential gcc unzip wget lz4 aria2 -y
+
+        # Проверка архитектуры системы
+        ARCH=$(uname -m)
+        if [[ "$ARCH" == "aarch64" ]]; then
+            curl -L -o dkn-compute-node.zip https://github.com/firstbatchxyz/dkn-compute-launcher/releases/latest/download/dkn-compute-launcher-linux-arm64.zip
+        elif [[ "$ARCH" == "x86_64" ]]; then
+            curl -L -o dkn-compute-node.zip https://github.com/firstbatchxyz/dkn-compute-launcher/releases/latest/download/dkn-compute-launcher-linux-amd64.zip
+        else
+            echo -e "${RED}Не поддерживаемая архитектура системы: $ARCH${NC}"
+            exit 1
+        fi
+
+        # Распаковываем ZIP-файл и переходим в папку
+        unzip dkn-compute-node.zip
+        cd dkn-compute-node
+
+        # Запускаем приложение для ввода данных
+        ./dkn-compute-launcher
+        ;;
+    2)
+        echo -e "${BLUE}Запускаем ноду Dria...${NC}"
+
+        # Определяем имя текущего пользователя и его домашнюю директорию
+        USERNAME=$(whoami)
+        HOME_DIR=$(eval echo ~$USERNAME)
+
+        # Создание файла сервиса
+        sudo bash -c "cat <<EOT > /etc/systemd/system/dria.service
+[Unit]
+Description=Dria Compute Node Service
+After=network.target
+
+[Service]
+User=$USERNAME
+EnvironmentFile=$HOME_DIR/dkn-compute-node/.env
+ExecStart=$HOME_DIR/dkn-compute-node/dkn-compute-launcher
+WorkingDirectory=$HOME_DIR/dkn-compute-node/
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOT"
+
+        # Перезагрузка и старт сервиса
+        sudo systemctl daemon-reload
+        sleep 1
+        sudo systemctl enable dria
+        sudo systemctl start dria
+
+        # Заключительный вывод
+        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
+        echo -e "${YELLOW}Команда для проверки логов:${NC}"
+        echo "sudo journalctl -u dria -f"
+        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
+        echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
+        echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
+        sleep 2
+
+        # Проверка логов
+        sudo journalctl -u dria -f
+        ;;
+    3)
+        echo -e "${GREEN}Установлена актуальная версия${NC}"
+        ;;
+    4)
+        echo -e "${YELLOW}Изменение порта...${NC}"
+
+        # Остановка сервиса
+        sudo systemctl stop dria
+
+        # Запрашиваем новый порт у пользователя
+        echo -e "${YELLOW}Введите новый порт для Dria:${NC}"
+        read NEW_PORT
+
+        # Путь к файлу .env
+        ENV_FILE="$HOME/dkn-compute-node/.env"
+
+        # Обновляем порт в файле .env
+        sed -i "s|DKN_P2P_LISTEN_ADDR=/ip4/0.0.0.0/tcp/[0-9]*|DKN_P2P_LISTEN_ADDR=/ip4/0.0.0.0/tcp/$NEW_PORT|" "$ENV_FILE"
+
+        # Перезапуск сервиса
+        sudo systemctl daemon-reload
+        sudo systemctl start dria
+
+        # Заключительный вывод
+        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
+        echo -e "${YELLOW}Команда для проверки логов:${NC}"
+        echo "sudo journalctl -u dria -f"
+        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
+        echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
+        echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
+        sleep 2
+
+        # Проверка логов
+        sudo journalctl -u dria -f
+        ;;
+    5)
+        # Проверка логов
+        sudo journalctl -u dria -f
+        ;;
+    6)
+        echo -e "${BLUE}Удаление ноды Dria...${NC}"
+
+        # Остановка и удаление сервиса
+        sudo systemctl stop dria
+        sudo systemctl disable dria
+        sudo rm /etc/systemd/system/dria.service
+        sudo systemctl daemon-reload
+        sleep 2
+
+        # Удаление папки ноды
+        rm -rf $HOME/dkn-compute-node
+
+        echo -e "${GREEN}Нода Dria успешно удалена!${NC}"
+
+        # Завершающий вывод
+        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
+        echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
+        echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
+        sleep 1
+        ;;
+    *)
+        echo -e "${RED}Неверный выбор. Пожалуйста, введите номер от 1 до 6.${NC}"
+        ;;
+esac
