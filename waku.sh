b3cdd2d0236dd05c61e24b04d721c36b0cb49614
NOXU <noxu.space@gmail.com>
2024-12-15 21:38:14 +0200

    Update waku.sh

--- waku.sh
+++ waku.sh
@@ -101,8 +101,8 @@ curl -s https://raw.githubusercontent.com/noxuspace/cryptofortochka/main/logo_cl
             echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
             echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
             echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
-            sleep 2
-            docker-compose logs -f
+            sleep 1
+           
             ;;
 
         2)
