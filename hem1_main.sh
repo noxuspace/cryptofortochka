228efd248449025ac7502785e83d60c023fff0f7
NOXU <noxu.space@gmail.com>
2025-03-12 23:04:35 +0200

    Update hem1_main.sh

--- hem1_main.sh
+++ hem1_main.sh
@@ -53,21 +53,22 @@ case $choice in
         cd hemi-main
 
         # Создание tBTC кошелька
-        ./keygen -secp256k1 -json  > ~/popm-address.json
+        ./keygen -secp256k1 -json  > popm-address.json
 
         # Вывод содержимого файла popm-address.json
         echo -e "${RED}Сохраните эти данные в надежное место:${NC}"
-        cat ~/popm-address.json
-        echo -e "${PURPLE}Ваш pubkey_hash — это ваш tBTC адрес, на который нужно запросить тестовые токены в Discord проекта.${NC}"
+        cat popm-address.json
+        echo -e "${PURPLE}Ваш pubkey_hash — это ваш адрес, на который нужно отправить BTC.${NC}"
 
         echo -e "${YELLOW}Введите ваш приватный ключ от кошелька:${NC} "
         read PRIV_KEY
-        echo -e "${YELLOW}Укажите желаемый размер комиссии (минимум 50):${NC} "
+        echo -e "${YELLOW}Укажите желаемый размер комиссии (рекомендуем 2-3):${NC} "
         read FEE
 
         echo "POPM_BTC_PRIVKEY=$PRIV_KEY" > popmd.env
         echo "POPM_STATIC_FEE=$FEE" >> popmd.env
-        echo "POPM_BFG_URL=wss://testnet.rpc.hemi.network/v1/ws/public" >> popmd.env
+        echo "POPM_BFG_URL=wss://pop.hemi.network/v1/ws/public" >> popmd.env
+        echo "POPM_BTC_CHAIN_NAME=mainnet" >> popmd.env
         sleep 1
 
         # Определяем имя текущего пользователя и его домашнюю директорию
@@ -80,16 +81,16 @@ else
 fi
 
 # Создаем или обновляем файл сервиса с использованием определенного имени пользователя и домашней директории
-cat <<EOT | sudo tee /etc/systemd/system/hemi.service > /dev/null
+cat <<EOT | sudo tee /etc/systemd/system/hemi-main.service > /dev/null
 [Unit]
-Description=PopMD Service
+Description=PopMD Main Service
 After=network.target
 
 [Service]
 User=$USERNAME
-EnvironmentFile=$HOME_DIR/hemi/popmd.env
-ExecStart=$HOME_DIR/hemi/popmd
-WorkingDirectory=$HOME_DIR/hemi/
+EnvironmentFile=$HOME_DIR/hemi-main/popmd.env
+ExecStart=$HOME_DIR/hemi-main/popmd
+WorkingDirectory=$HOME_DIR/hemi-main/
 Restart=on-failure
 
 [Install]
@@ -99,11 +100,11 @@ EOT
         # Обновление сервисов и включение hemi
         sudo systemctl daemon-reload
         sudo systemctl restart systemd-journald
-        sudo systemctl enable hemi
+        sudo systemctl enable hemi-main
         sleep 1
 
         # Запуск ноды
-        sudo systemctl start hemi
+        sudo systemctl start hemi-main
 
         # Заключительный вывод
         echo -e "${GREEN}Установка завершена и нода запущена!${NC}"
@@ -111,63 +112,57 @@ EOT
         # Завершающий вывод
         echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
         echo -e "${YELLOW}Команда для проверки логов:${NC}" 
-        echo "sudo journalctl -u hemi -f"
+        echo "sudo journalctl -u hemi-main -f"
         echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
         echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
         echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
         sleep 2
-        sudo journalctl -u hemi -f
+        sudo journalctl -u hemi-main -f
         ;;
     2)
         echo -e "${BLUE}Обновляем ноду Hemi...${NC}"
 
-        # Находим все сессии screen, содержащие "hemi"
-        SESSION_IDS=$(screen -ls | grep "hemi" | awk '{print $1}' | cut -d '.' -f 1)
-
-        # Если сессии найдены, удаляем их
-        if [ -n "$SESSION_IDS" ]; then
-            echo -e "${BLUE}Завершение сессий screen с идентификаторами: $SESSION_IDS${NC}"
-            for SESSION_ID in $SESSION_IDS; do
-                screen -S "$SESSION_ID" -X quit
-            done
-        else
-            echo -e "${BLUE}Сессии screen для ноды Hemi не найдены, начинаем обновление${NC}"
-        fi
-
         # Проверка существования сервиса
-        if systemctl list-units --type=service | grep -q "hemi.service"; then
-            sudo systemctl stop hemi.service
-            sudo systemctl disable hemi.service
-            sudo rm /etc/systemd/system/hemi.service
+        if systemctl list-units --type=service | grep -q "hemi-main.service"; then
+            sudo systemctl stop hemi-main.service
+            sudo systemctl disable hemi-main.service
+            sudo rm /etc/systemd/system/hemi-main.service
             sudo systemctl daemon-reload
         else
-            echo -e "${BLUE}Сервис hemi.service не найден, продолжаем обновление.${NC}"
+            echo -e "${BLUE}Сервис hemi-main.service не найден, продолжаем обновление.${NC}"
         fi
         sleep 1
 
         # Удаление папки с бинарниками, содержащими "hemi" в названии
         echo -e "${BLUE}Удаляем старые файлы ноды...${NC}"
-        rm -rf *hemi*
+        rm -rf hemi-main
 
         # Установка бинарника
         echo -e "${BLUE}Загружаем бинарник Hemi...${NC}"
-        curl -L -O https://github.com/hemilabs/heminetwork/releases/download/v0.11.5/heminetwork_v0.11.5_linux_amd64.tar.gz
+        curl -L -O https://github.com/hemilabs/heminetwork/releases/download/v1.0.0/heminetwork_v1.0.0_linux_amd64.tar.gz
 
         # Создание директории и извлечение бинарника
-        mkdir -p hemi
-        tar --strip-components=1 -xzvf heminetwork_v0.11.5_linux_amd64.tar.gz -C hemi
-        cd hemi
+        mkdir -p hemi-main
+        tar --strip-components=1 -xzvf heminetwork_v1.0.0_linux_amd64.tar.gz -C hemi-main
+        cd hemi-main
 
-        # Запрос приватного ключа и комиссии
+        # Создание tBTC кошелька
+        ./keygen -secp256k1 -json  > popm-address.json
+
+        # Вывод содержимого файла popm-address.json
+        echo -e "${RED}Сохраните эти данные в надежное место:${NC}"
+        cat popm-address.json
+        echo -e "${PURPLE}Ваш pubkey_hash — это ваш адрес, на который нужно отправить BTC.${NC}"
+
         echo -e "${YELLOW}Введите ваш приватный ключ от кошелька:${NC} "
         read PRIV_KEY
-        echo -e "${YELLOW}Укажите желаемый размер комиссии (минимум 50):${NC} "
+        echo -e "${YELLOW}Укажите желаемый размер комиссии (рекомендуем 2-3):${NC} "
         read FEE
 
-        # Создание файла popmd.env
         echo "POPM_BTC_PRIVKEY=$PRIV_KEY" > popmd.env
         echo "POPM_STATIC_FEE=$FEE" >> popmd.env
-        echo "POPM_BFG_URL=wss://testnet.rpc.hemi.network/v1/ws/public" >> popmd.env
+        echo "POPM_BFG_URL=wss://pop.hemi.network/v1/ws/public" >> popmd.env
+        echo "POPM_BTC_CHAIN_NAME=mainnet" >> popmd.env
         sleep 1
 
         # Определяем имя текущего пользователя и его домашнюю директорию
@@ -180,16 +175,16 @@ else
 fi
 
 # Создаем или обновляем файл сервиса с использованием определенного имени пользователя и домашней директории
-cat <<EOT | sudo tee /etc/systemd/system/hemi.service > /dev/null
+cat <<EOT | sudo tee /etc/systemd/system/hemi-main.service > /dev/null
 [Unit]
-Description=PopMD Service
+Description=PopMD Main Service
 After=network.target
 
 [Service]
 User=$USERNAME
-EnvironmentFile=$HOME_DIR/hemi/popmd.env
-ExecStart=$HOME_DIR/hemi/popmd
-WorkingDirectory=$HOME_DIR/hemi/
+EnvironmentFile=$HOME_DIR/hemi-main/popmd.env
+ExecStart=$HOME_DIR/hemi-main/popmd
+WorkingDirectory=$HOME_DIR/hemi-main/
 Restart=on-failure
 
 [Install]
@@ -199,91 +194,77 @@ EOT
         # Обновление сервисов и включение hemi
         sudo systemctl daemon-reload
         sudo systemctl restart systemd-journald
-        sudo systemctl enable hemi
+        sudo systemctl enable hemi-main
         sleep 1
 
         # Запуск ноды
-        sudo systemctl start hemi
+        sudo systemctl start hemi-main
 
         # Заключительный вывод
-        echo -e "${GREEN}Нода обновлена и запущена!${NC}"
+        echo -e "${GREEN}Установка завершена и нода запущена!${NC}"
 
         # Завершающий вывод
         echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
         echo -e "${YELLOW}Команда для проверки логов:${NC}" 
-        echo "sudo journalctl -u hemi -f"
+        echo "sudo journalctl -u hemi-main -f"
         echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
         echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
         echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
         sleep 2
-        sudo journalctl -u hemi -f
+        sudo journalctl -u hemi-main -f
         ;;
     3)
-        echo -e "${YELLOW}Укажите новое значение комиссии (минимум 50):${NC}"
+        echo -e "${YELLOW}Укажите новое значение комиссии (рекомендуем 2-3):${NC}"
         read NEW_FEE
 
-        # Проверка, что введенное значение не меньше 50
-        if [ "$NEW_FEE" -ge 50 ]; then
+        # Проверка, что введенное значение не меньше 1
+        if [ "$NEW_FEE" -ge 1 ]; then
             # Обновляем значение комиссии в файле popmd.env
-            sed -i "s/^POPM_STATIC_FEE=.*/POPM_STATIC_FEE=$NEW_FEE/" $HOME/hemi/popmd.env
+            sed -i "s/^POPM_STATIC_FEE=.*/POPM_STATIC_FEE=$NEW_FEE/" $HOME/hemi-main/popmd.env
             sleep 1
 
             # Перезапуск сервиса Hemi
-            sudo systemctl restart hemi
+            sudo systemctl restart hemi-main
 
             echo -e "${GREEN}Значение комиссии успешно изменено!${NC}"
 
             # Завершающий вывод
             echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
             echo -e "${YELLOW}Команда для проверки логов:${NC}" 
-            echo "sudo journalctl -u hemi -f"
+            echo "sudo journalctl -u hemi-main -f"
             echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
             echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
             echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
         else
-            echo -e "${RED}Ошибка: комиссия должна быть не меньше 50!${NC}"
+            echo -e "${RED}Ошибка: комиссия должна быть не меньше 1!${NC}"
         fi
+        sleep 2
+        sudo journalctl -u hemi-main -f
         ;;
 
     4)
         echo -e "${BLUE}Удаление ноды Hemi...${NC}"
 
-        # Находим все сессии screen, содержащие "hemi"
-        SESSION_IDS=$(screen -ls | grep "hemi" | awk '{print $1}' | cut -d '.' -f 1)
-
-        # Если сессии найдены, удаляем их
-        if [ -n "$SESSION_IDS" ]; then
-            echo -e "${BLUE}Завершение сессий screen с идентификаторами: $SESSION_IDS${NC}"
-            for SESSION_ID in $SESSION_IDS; do
-                screen -S "$SESSION_ID" -X quit
-            done
-        else
-            echo -e "${BLUE}Сессии screen для ноды Hemi не найдены, продолжаем удаление${NC}"
-        fi
-
         # Остановка и удаление сервиса Hemi
-        sudo systemctl stop hemi.service
-        sudo systemctl disable hemi.service
-        sudo rm /etc/systemd/system/hemi.service
+        sudo systemctl stop hemi-main.service
+        sudo systemctl disable hemi-main.service
+        sudo rm /etc/systemd/system/hemi-main.service
         sudo systemctl daemon-reload
         sleep 1
 
         # Удаление папки с названием, содержащим "hemi"
         echo -e "${BLUE}Удаляем файлы ноды Hemi...${NC}"
-        rm -rf *hemi*
+        rm -rf hemi-main
         
         echo -e "${GREEN}Нода Hemi успешно удалена!${NC}"
 
         # Завершающий вывод
         echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-        echo -e "${YELLOW}Команда для проверки логов:${NC}" 
-        echo "sudo journalctl -u hemi -f"
-        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
         echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
         echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
         ;;
     5)
-        sudo journalctl -u hemi -f
+        sudo journalctl -u hemi-main -f
         ;;
         
 esac
