fda5ad394e854093ac0a2e0b51805a294ddf3a5e
NOXU <noxu.space@gmail.com>
2025-03-27 15:32:58 +0200

    Create wl.sh

---
+++ soundness/wl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if ! command -v docker &> /dev/null; then
+            echo -e "${YELLOW}Docker не установлен. Устанавливаем Docker...${NC}"
+            sudo apt install docker.io -y
+
+https://github.com/icodragon/soundness-layer -b feat/docker-soundness-cli && cd soundness-layer/soundness-cli
+sleep 1
+
+docker compose build
+sleep 2
+
+docker compose run --rm soundness-cli generate-key --name my-key
