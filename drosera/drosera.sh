1ef2ec7f9f51ff7dccc7a5f2f42f8da51c2f4d2f
NOXU <noxu.space@gmail.com>
2025-04-15 11:29:02 +0300

    Update drosera.sh

--- drosera/drosera.sh
+++ drosera/drosera.sh
@@ -72,109 +72,27 @@ case $choice in
         
         # Заключительное сообщение
         echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-        echo -e "${YELLOW}Вернитесь к текстовому гайду!${NC}"
+        echo -e "${YELLOW}Команда для проверки логов:${NC}"
+        echo "journalctl -u drosera.service -f"
         echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
+        echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
+        echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
         sleep 2
+        journalctl -u drosera.service -f
         ;;
     5)
-        echo -e "${BLUE}Установка ноды в мейннете...${NC}"
-
-        # Обновление и установка зависимостей
-        sudo apt update -y
-        sudo apt upgrade -y
-        sudo apt install -y curl git jq lz4 build-essential unzip
-
-        # Проверка наличия Docker и Docker Compose
-        if ! command -v docker &> /dev/null; then
-            echo -e "${YELLOW}Docker не установлен. Устанавливаем Docker...${NC}"
-            sudo apt install docker.io -y
-        fi
-
-        if ! command -v docker-compose &> /dev/null; then
-            echo -e "${YELLOW}Docker Compose не установлен. Устанавливаем Docker Compose...${NC}"
-            sudo curl -L "https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-            sudo chmod +x /usr/local/bin/docker-compose
-        fi
-
-                # Создание папки elixir-main и загрузка конфигурационного файла
-        mkdir -p $HOME/elixir-main
-        cd $HOME/elixir-main
-        wget https://files.elixir.finance/validator.env
-
-        # Запрос данных у пользователя
-        echo -e "${YELLOW}Введите IP-адрес сервера:${NC}"
-        read STRATEGY_EXECUTOR_IP_ADDRESS
-
-        echo -e "${YELLOW}Введите имя валидатора:${NC}"
-        read STRATEGY_EXECUTOR_DISPLAY_NAME
-
-        echo -e "${YELLOW}Введите адрес EVM:${NC}"
-        read STRATEGY_EXECUTOR_BENEFICIARY
-
-        echo -e "${YELLOW}Введите приватный ключ EVM:${NC}"
-        read SIGNER_PRIVATE_KEY
-
-        # Обновление конфигурационного файла
-        sed -i 's|ENV=prod|ENV=prod|' validator.env
-        echo "STRATEGY_EXECUTOR_IP_ADDRESS=$STRATEGY_EXECUTOR_IP_ADDRESS" >> validator.env
-        echo "STRATEGY_EXECUTOR_DISPLAY_NAME=$STRATEGY_EXECUTOR_DISPLAY_NAME" >> validator.env
-        echo "STRATEGY_EXECUTOR_BENEFICIARY=$STRATEGY_EXECUTOR_BENEFICIARY" >> validator.env
-        echo "SIGNER_PRIVATE_KEY=$SIGNER_PRIVATE_KEY" >> validator.env
-
-        # Установка Docker-образа
-        docker pull elixirprotocol/validator --platform linux/amd64
-
-        # Запуск контейнера
-        docker run --name elixir-main --env-file validator.env --platform linux/amd64 -p 17691:17690 --restart unless-stopped elixirprotocol/validator
-
-        # Заключительное сообщение
-        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-        echo -e "${YELLOW}Команда для проверки логов:${NC}"
-        echo "docker logs -f elixir-main"
-        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-        echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
-        echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
-        sleep 2
+        echo -e "${GREEN}У вас актуальная версия ноды Drosera!${NC}"
         ;;
     6)
-        echo -e "${BLUE}Обновление ноды в мейннете...${NC}"
-
-        # Остановка и удаление контейнера
-        cd $HOME/elixir-main
-        docker ps -a | grep " elixir-main$" | awk '{print $1}' | xargs docker stop
-        docker ps -a | grep " elixir-main$" | awk '{print $1}' | xargs docker rm
-
-        # Установка новой версии
-        docker pull elixirprotocol/validator --platform linux/amd64
-
-        # Запуск контейнера
-        docker run --name elixir-main --env-file validator.env --platform linux/amd64 -p 17691:17690 --restart unless-stopped elixirprotocol/validator
-
-        # Заключительное сообщение
-        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-        echo -e "${YELLOW}Команда для проверки логов:${NC}"
-        echo "docker logs -f elixir-main"
-        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-        echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
-        echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
-        sleep 2
+        journalctl -u drosera.service -f
         ;;
     7)
-        # Проверка логов
-        docker logs -f elixir-main
+        sudo systemctl restart drosera && journalctl -u drosera.service -f
         ;;
     8)
-        echo -e "${BLUE}Удаление ноды в мейннете...${NC}"
+        echo -e "${BLUE}Удаление ноды Drosera...${NC}"
 
-        # Остановка и удаление контейнера
-        cd $HOME/elixir-main
-        docker ps -a | grep " elixir-main$" | awk '{print $1}' | xargs docker stop
-        docker ps -a | grep " elixir-main$" | awk '{print $1}' | xargs docker rm
-
-        # Удаление папки проекта
-        rm -rf $HOME/elixir-main
-
-        echo -e "${GREEN}Нода успешно удалена!${NC}"
+        
 
         # Заключительное сообщение
         echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
