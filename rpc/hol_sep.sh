65c9e0041b069aa8edcef86f63f2fe87a84a463b
NOXU <noxu.space@gmail.com>
2025-04-24 23:57:21 +0300

    Update hol_sep.sh

--- rpc/hol_sep.sh
+++ rpc/hol_sep.sh
@@ -37,14 +37,45 @@ curl -s https://raw.githubusercontent.com/noxuspace/cryptofortochka/main/logo_cl
             sudo ufw allow 22:65535/tcp
             sudo ufw allow 22:65535/udp
             sudo ufw deny out from any to 10.0.0.0/8
-            #sudo ufw deny out from any to 172.16.0.0/12
             sudo ufw deny out from any to 192.168.0.0/16
             sudo ufw deny out from any to 100.64.0.0/10
             sudo ufw deny out from any to 198.18.0.0/15
             sudo ufw deny out from any to 169.254.0.0/16
             sudo ufw --force enable
 
+            curl -s https://raw.githubusercontent.com/noxuspace/cryptofortochka/main/docker/docker_main.sh | bash &>/dev/null
+
+            if ! id "geth_holesky" &>/dev/null; then
+                useradd -s /bin/bash -m geth_holesky
+                sudo usermod -aG sudo geth_holesky
+                sudo usermod -aG docker geth_holesky
+                sudo passwd geth_holesky
+            else
+                echo "User 'geth_holesky' already exists."
+            fi
             
+            if [ ! -d "/home/geth_holesky/eth-docker" ]; then
+                    sudo -u geth_holesky git clone https://github.com/eth-educators/eth-docker.git /home/geth_holesky/eth-docker
+                else
+                    echo "Directory '/home/geth_holesky/eth-docker' already exists."
+            fi
+            
+            
+            sudo -u geth_holesky cp /home/geth_holesky/eth-docker/default.env /home/geth_holesky/eth-docker/.env
+            sudo -u geth_holesky sed -i 's/COMPOSE_FILE=.*/COMPOSE_FILE=prysm-cl-only.yml:geth.yml:grafana.yml:grafana-shared.yml:el-shared.yml/g' /home/geth_holesky/eth-docker/.env
+            sudo -u geth_holesky sed -i 's/EL_P2P_PORT=.*/EL_P2P_PORT=40303/g' /home/geth_holesky/eth-docker/.env
+            sudo -u geth_holesky sed -i 's/CL_P2P_PORT=.*/CL_P2P_PORT=49000/g' /home/geth_holesky/eth-docker/.env
+            sudo -u geth_holesky sed -i 's/CL_QUIC_PORT=.*/CL_QUIC_PORT=49001/g' /home/geth_holesky/eth-docker/.env
+            sudo -u geth_holesky sed -i 's/GRAFANA_PORT=.*/GRAFANA_PORT=43000/g' /home/geth_holesky/eth-docker/.env
+            sudo -u geth_holesky sed -i 's/EL_RPC_PORT=.*/EL_RPC_PORT=48545/g' /home/geth_holesky/eth-docker/.env
+            sudo -u geth_holesky sed -i 's/EL_WS_PORT=.*/EL_WS_PORT=48546/g' /home/geth_holesky/eth-docker/.env
+            sudo -u geth_holesky sed -i 's/NETWORK=.*/NETWORK=holesky/g' /home/geth_holesky/eth-docker/.env
+            sudo -u geth_holesky sed -i 's/CHECKPOINT_SYNC_URL=.*/CHECKPOINT_SYNC_URL=\"https:\/\/holesky.beaconstate.info\"/g' /home/geth_holesky/eth-docker/.env
+            sudo -u geth_holesky sed -i 's/FEE_RECIPIENT=.*/FEE_RECIPIENT=0xd9264738573E25CB9149de0708b36527d56B59bd/g' /home/geth_holesky/eth-docker/.env
+            
+            
+            export COMPOSE_PROJECT_NAME=holesky
+            sudo -u geth_holesky /home/geth_holesky/eth-docker/ethd up
             ;;
 
         2)
