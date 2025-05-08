9f7751b1fad81726869ca1782e30899ada8b533c
NOXU <noxu.space@gmail.com>
2025-05-08 14:51:58 +0300

    Update aztec.sh

--- aztec/aztec.sh
+++ aztec/aztec.sh
@@ -36,6 +36,7 @@ case $choice in
     1)
         echo -e "${BLUE}Установка зависимостей...${NC}"
         sudo apt-get update && sudo apt-get upgrade -y
+        sudo apt install iptables-persistent
         sudo apt install curl iptables build-essential git wget lz4 jq make gcc nano automake autoconf tmux htop nvme-cli libgbm1 pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev  -y
         
         # 1. Установка Docker, если не установлен
