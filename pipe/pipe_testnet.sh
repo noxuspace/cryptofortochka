1ec66a3f7fbe226fa27b7ee601b13ae76878b8b4
NOXU <noxu.space@gmail.com>
2025-05-12 19:30:24 +0300

    Update pipe_testnet.sh

--- pipe/pipe_testnet.sh
+++ pipe/pipe_testnet.sh
@@ -125,7 +125,7 @@ net.ipv4.tcp_rmem = 4096 87380 16777216
 net.core.wmem_max = 16777216
 net.core.rmem_max = 16777216
 EOL'
-    sudo sysctl --system
+    sudo sysctl -p /etc/sysctl.d/99-popcache.conf
 
     # Лимиты открытых файлов
     sudo bash -c 'cat > /etc/security/limits.d/popcache.conf << EOL
