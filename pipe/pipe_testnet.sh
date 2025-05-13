14f1766c7ff9ba8a9d9184be1c8da3cfe918007a
NOXU <noxu.space@gmail.com>
2025-05-13 22:48:24 +0300

    Update pipe_testnet.sh

--- pipe/pipe_testnet.sh
+++ pipe/pipe_testnet.sh
@@ -160,11 +160,11 @@ EOL'
 EOL
 
     # Удаляем существующие правила, если они уже есть
-    sudo iptables -D INPUT -p tcp --dport 443 -j ACCEPT 2>/dev/null
-    sudo iptables -D INPUT -p tcp --dport 80 -j ACCEPT  2>/dev/null
-    sudo iptables -D INPUT -p tcp --dport 8003 -j ACCEPT 2>/dev/null
-    sudo iptables -D INPUT -p tcp --dport 8071 -j ACCEPT 2>/dev/null
-    sudo iptables -D INPUT -p tcp --dport 17443 -j ACCEPT 2>/dev/null
+    #sudo iptables -D INPUT -p tcp --dport 443 -j ACCEPT 2>/dev/null
+    #sudo iptables -D INPUT -p tcp --dport 80 -j ACCEPT  2>/dev/null
+    #sudo iptables -D INPUT -p tcp --dport 8003 -j ACCEPT 2>/dev/null
+    #sudo iptables -D INPUT -p tcp --dport 8071 -j ACCEPT 2>/dev/null
+    #sudo iptables -D INPUT -p tcp --dport 17443 -j ACCEPT 2>/dev/null
     
     # Настройка iptables
     sudo iptables -I INPUT -p tcp --dport 443 -j ACCEPT
