c5afd0e487fba094e954ebca1c4472f12ec05428
NOXU <noxu.space@gmail.com>
2025-05-26 17:21:12 +0300

    Create blockcast.sh

---
+++ blockcast/blockcast.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+# Цвета текста
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m' # Нет цвета (сброс цвета)
+
+# Проверка наличия curl и установка, если не установлен
+if ! command -v curl &> /dev/null; then
+    sudo apt update
+    sudo apt install curl -y
+fi
+sleep 1
+
+# Отображаем логотип
+curl -s https://raw.githubusercontent.com/noxuspace/cryptofortochka/main/logo_club.sh | bash
+
+# Меню
+echo -e "${YELLOW}Выберите действие:${NC}"
+echo -e "${CYAN}1) Установка ноды${NC}"
+echo -e "${CYAN}2) Регистрация ноды${NC}"
+echo -e "${CYAN}3) Просмотр логов${NC}"
+echo -e "${CYAN}4) Рестарт ноды${NC}"
+echo -e "${CYAN}5) Обновление ноды${NC}"
+echo -e "${CYAN}6) Удаление ноды${NC}"
+
+echo -e "${YELLOW}Введите номер:${NC} "
+read choice
+
+case $choice in
+    1)
+        echo -e "${BLUE}Установка зависимостей...${NC}"
+        sudo apt-get update && sudo apt-get upgrade -y
+        sudo apt install iptables-persistent
+        sudo apt install curl iptables build-essential git wget lz4 jq make gcc nano automake autoconf tmux htop nvme-cli libgbm1 pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev  -y
+        
+      # Проверка наличия iptables и установка, если отсутствует
+        if ! command -v iptables &> /dev/null; then
+          sudo apt-get update -y
+          sudo apt-get install -y iptables
+        fi
+
+        sudo apt update
+        sudo apt install -y iptables-persistent
+
+        echo -e "${BLUE}Проверяем Docker и Docker-Compose...${NC}"
+        bash <(curl -fsSL https://raw.githubusercontent.com/noxusspace/cryptofortochka/main/docker/docker_main.sh)
+
+      
+
+        cd ~
+        # Завершающий вывод
+        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
+        echo -e "${YELLOW}Команда для проверки логов:${NC}" 
+        echo "docker logs --tail 100 -f aztec-sequencer"
+        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
+        echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
+        echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
+        sleep 2
+        docker logs --tail 100 -f aztec-sequencer     
+        ;;
+    2)
+        # создаём временный файл
+        tmpf=$(mktemp) && \
+        # скачиваем role.sh в этот файл
+        curl -fsSL https://raw.githubusercontent.com/noxuspace/cryptofortochka/main/aztec/role.sh > "$tmpf" && \
+        # исполняем его
+        bash "$tmpf" && \
+        # удаляем временный файл
+        rm -f "$tmpf"
+        ;;
+    3)
+        tmpf=$(mktemp) &&
+        curl -fsSL https://raw.githubusercontent.com/noxuspace/cryptofortochka/main/aztec/validator.sh >"$tmpf" &&
+        bash "$tmpf" &&
+        rm -f "$tmpf"
+        ;;
+    4)
+        echo -e "${BLUE}Обновление ноды Aztec...${NC}"
+        # 1) Подтягиваем новую версию образа
+        docker pull aztecprotocol/aztec:0.87.2
+
+        # 2) Останавливаем и удаляем старый контейнер (тома и .evm сохранятся)
+        docker stop aztec-sequencer
+        docker rm aztec-sequencer
+
+        rm -rf "$HOME/my-node/node/"*
+
+        # 3) Запускаем контейнер заново с теми же параметрами, но новым тегом
+        docker run -d \
+          --name aztec-sequencer \
+          --network host \
+          --entrypoint /bin/sh \
+          --env-file "$HOME/aztec-sequencer/.evm" \
+          -e DATA_DIRECTORY=/data \
+          -e LOG_LEVEL=debug \
+          -v "$HOME/my-node/node":/data \
+          aztecprotocol/aztec:0.87.2 \
+          -c 'node --no-warnings /usr/src/yarn-project/aztec/dest/bin/index.js \
+            start --network alpha-testnet --node --archiver --sequencer'
+
+
+        # Завершающий вывод
+        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
+        echo -e "${YELLOW}Команда для проверки логов:${NC}" 
+        echo "docker logs --tail 100 -f aztec-sequencer"
+        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
+        echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
+        echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
+        sleep 2
+        docker logs --tail 100 -f aztec-sequencer
+        ;;
+    5)
+        docker logs --tail 100 -f aztec-sequencer
+        ;;
+    6)
+        docker restart aztec-sequencer
+        docker logs --tail 100 -f aztec-sequencer
+        ;;
+    7)
+        echo -e "${BLUE}Удаление ноды Aztec...${NC}"
+        docker stop aztec-sequencer
+        docker rm aztec-sequencer
+
+        rm -rf "$HOME/my-node/node/"*
+        rm -rf $HOME/aztec-sequencer
+        
+        # Заключительное сообщение
+        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
+        echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
+        echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
+        sleep 1
+        ;;
+    *)
+        echo -e "${RED}Неверный выбор. Пожалуйста, выберите пункт из меню.${NC}"
+        ;;
+esac
+
+
+       
