7daf4f61490483ff2f69920720314714c81cbda2
NOXU <noxu.space@gmail.com>
2025-06-10 23:50:08 +0300

    Update old_test2024.sh

--- old_test2024.sh
+++ old_test2024.sh
@@ -7,7 +7,9 @@ apt install -y wget screen curl sudo
 curl -L https://github.com/cysic-labs/cysic-phase3/releases/download/v1.0.0/setup_prover.sh > ~/setup_prover.sh && \
 bash ~/setup_prover.sh 0x66755D560e4447C7D2a50AcBB119d4896B6eAad3 https://eth-mainnet.g.alchemy.com/v2/PQz9FYqFUrRfkkO6UPjRkxW66KGrX90U
 
-for i in {1..10}; do
+for i in {1..20}; do cp -a --force /root/cysic-prover/ /root/cysic-prover$i/; done
+
+for i in {1..20}; do
   SESSION="prover$i"
   DIR="$HOME/cysic-prover$i"
 
