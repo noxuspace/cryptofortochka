b895ddaabcd5e05056bc5ec7c5fa52f04d4bd4df
NOXU <noxu.space@gmail.com>
2025-07-28 14:57:32 +0300

    Update cadet_drosera.sh

--- drosera/cadet_drosera.sh
+++ drosera/cadet_drosera.sh
@@ -43,7 +43,7 @@ interface IMockResponse {
 }
 
 contract Trap is ITrap {
-    address public constant RESPONSE_CONTRACT = 0x4608Afa7f277C8E0BE232232265850d1cDeB600E;
+    address public constant RESPONSE_CONTRACT = 0x25E2CeF36020A736CF8a4D2cAdD2EBE3940F4608;
     string constant discordName = "${DISCORD}"; // add your discord name here
 
     function collect() external view returns (bytes memory) {
@@ -66,7 +66,7 @@ EOF
 
 # Обновляем в drosera.toml нужные поля
 sed -i 's|^path = .*|path = "out/Trap.sol/Trap.json"|' drosera.toml
-sed -i 's|^response_contract = .*|response_contract = "0x4608Afa7f277C8E0BE232232265850d1cDeB600E"|' drosera.toml
+sed -i 's|^response_contract = .*|response_contract = "0x25E2CeF36020A736CF8a4D2cAdD2EBE3940F4608"|' drosera.toml
 sed -i 's|^response_function = .*|response_function = "respondWithDiscordName(string)"|' drosera.toml
 
 # Собираем контракт
