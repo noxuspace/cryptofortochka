bb3f7f6c33583aecc33df8d7896ca7d403e996b5
NOXU <noxu.space@gmail.com>
2026-01-21 16:27:03 +0200

    Update arcium.sh

--- arcium/arcium.sh
+++ arcium/arcium.sh
@@ -707,7 +707,8 @@ PY
         -e NODE_KEYPAIR_FILE=/usr/arx-node/node-keys/node_keypair.json \
         -e OPERATOR_KEYPAIR_FILE=/usr/arx-node/node-keys/operator_keypair.json \
         -e CALLBACK_AUTHORITY_KEYPAIR_FILE=/usr/arx-node/node-keys/callback_authority_keypair.json \
-        -e BLS_PRIVATE_KEY_FILE=/usr/arx-node/node-keys/bls-keypair.json \
+        -e BLS_PRIVATE_KEY_FILE=/usr/arx-node/node-keys/bls-keypair.bin \
+        #-e BLS_PRIVATE_KEY_FILE=/usr/arx-node/node-keys/bls-keypair.json \
         -e NODE_CONFIG_PATH=/usr/arx-node/arx/node_config.toml \
         -e X25519_PRIVATE_KEY_FILE=/usr/arx-node/node-keys/x25519-key.pem \
         -v "$WORKDIR/x25519-key.pem:/usr/arx-node/node-keys/x25519-key.pem:ro" \
@@ -717,7 +718,6 @@ PY
         -v "$CALLBACK_KP:/usr/arx-node/node-keys/callback_authority_keypair.json:ro" \
         -v "$IDENTITY_PEM:/usr/arx-node/node-keys/node_identity.pem:ro" \
         -v "$BLS_BIN:/usr/arx-node/node-keys/bls-keypair.bin:ro" \
-        -e BLS_PRIVATE_KEY_FILE=/usr/arx-node/node-keys/bls-keypair.bin \
         #-v "$BLS_JSON:/usr/arx-node/node-keys/bls-keypair.json:ro" \
         -v "$LOGS_DIR:/usr/arx-node/logs" \
         -p 8080:8080 \
