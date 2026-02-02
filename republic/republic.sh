e744d852390c7e21828efa3690b6c9702141aeb7
NOXU <noxu.space@gmail.com>
2026-02-02 14:48:39 +0200

    Update republic.sh

--- republic/republic.sh
+++ republic/republic.sh
@@ -197,7 +197,7 @@ EOF
       cat > /tmp/validator.json <<EOF
 {
   "pubkey": $PUBKEY,
-  "amount": "1000000000000000000000arai",
+  "amount": "1000000000000000000arai",
   "moniker": "${MONIKER:-my-validator}",
   "identity": "",
   "website": "",
